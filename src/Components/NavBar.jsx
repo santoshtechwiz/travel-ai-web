da8d60d7dc50e65f93383d723e713305656f2db8
Santosh Kumar Singh <SANSI@orsted.com>
2024-01-01 14:02:58 +0800

    Menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -15,22 +15,37 @@ const NavBar = () => {
       <div className="flex items-center justify-between">
         <div className="text-2xl font-bold">
           <Link to="/" className="text-black">
-           <div>
-            <span className="font-bold text-yellow-700 mr-1 font-extrabold inline-block mt-2">Trip</span>
-             <span className="font-bold text-yellow-950 border-b-4 border-yellow-800 dark:border-yellow-500">Crafts</span></div>
+            <div>
+              <span className="font-bold text-yellow-700 mr-1 font-extrabold inline-block mt-2">
+                Trip
+              </span>
+              <span className="font-bold text-yellow-950 border-b-4 border-yellow-800 dark:border-yellow-500">
+                Crafts
+              </span>
+            </div>
           </Link>
         </div>
-        <div className="flex items-center space-x-12 z-50">
-          <Link to="/plan" className="font-semibold">
+        <div className="flex flex-col md:flex-row justify-start items-center space-x-12 z-50">
+          <Link
+            to="/plan"
+            className="font-semibold hover:border-b-4 border-yellow-800 border-yellow-500 mb-2 md:mb-0"
+          >
             Plan Your Trip
           </Link>
-          <Link to="/wonder" className="font-semibold">
+          <Link
+            to="/wonder"
+            className="font-semibold hover:border-b-4 border-yellow-800 border-yellow-500 mb-2 md:mb-0"
+          >
             Explore The Wonder
           </Link>
-          <Link to="/about" className="font-semibold">
+          <Link
+            to="/about"
+            className="font-semibold hover:border-b-4 border-yellow-800 border-yellow-500 mb-2 md:mb-0"
+          >
             About
           </Link>
         </div>
+
         <Link to="/">
           <img
             src={Decore}
